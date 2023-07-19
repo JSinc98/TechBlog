c6728c14291bbdda6971df095338c396197e957b
JSinc98 <josh.r.sinclair@gmail.com>
2023-07-18 20:21:13 -0400

    attempting to fix login and signup

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -6,13 +6,13 @@ const signup = async (event) => {
     const password = document.querySelector('#password-signup').value.trim();
 
     if (username && password) {
-        const response = await fetch('/api/user/signup', {
+        const response = await fetch('/api/user', {
           method: 'POST',
           body: JSON.stringify({ username, password }),
           headers: { 'Content-Type': 'application/json' },
         });
     
-        if (response) {
+        if (response.ok) {
           document.location.replace('/dashboard');
         } else {
           alert('Could not sign up, please try again');
